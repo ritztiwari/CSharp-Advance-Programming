2c23df8c804ee7a14c121510c2f4c5a2f68e23ce
Rishabh Tiwari <mrishabh532@gmail.com>
2026-01-19 16:02:02 +0530

    [Rishabh]. Add program.cs to run all the practice quesions in lamdba and LINQ functions.

---
+++ Lambda-LINQ/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using CSharp_Advance_Programming.Lambda_LINQ.EmployeeManagement;
+using CSharp_Advance_Programming.Lambda_LINQ.NamesManagement;
+using CSharp_Advance_Programming.Lambda_LINQ.ProductManagement;
+using CSharp_Advance_Programming.Lambda_LINQ.StudentManagement;
+
+namespace CSharp_Advance_Programming
+{
+    internal class Program
+    {
+        internal static void Main(string[] args)
+        {
+            // EmployeeMain.Exexute();
+
+            // Names.Execute();
+
+            // ProductMain.Execute();
+
+            StudentMain.Execute();
+        }
+    }
+}
