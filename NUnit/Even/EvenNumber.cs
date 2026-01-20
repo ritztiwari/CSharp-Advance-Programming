24464f6310110a5e6ba5e0f0ecda6b5a6996fc9b
Rishabh Tiwari <mrishabh532@gmail.com>
2026-01-20 16:51:33 +0530

    [Rishabh]. Add Even to check the given number and user is Even or odd by using test case for that using NUnit.

---
+++ NUnit/Even/EvenNumber.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Globalization;
+
+namespace CSharp_Advance_Programming.NUnit.Even
+{
+    internal class EvenNumber
+    {
+        internal bool FindEven(int num)
+        {
+            return num % 2 == 0;
+        }
+    }
+}
---
+++ NUnit/Even/EvenTest.cs
@@ -0,0 +1,26 @@
+﻿using CSharp_Advance_Programming.NUnit.Even;
+
+public class Tests
+{
+    private EvenNumber _evenNumber;
+
+    [SetUp]
+    public void Setup()
+    {
+        _evenNumber = new EvenNumber();
+    }
+
+    //testing the multiple inputs on the same method.
+    //it checks the number is even or not.
+    [TestCase(2, true)]
+    [TestCase(4, true)]
+    [TestCase(6, true)]
+    [TestCase(7, false)]
+    [TestCase(9, false)]
+    public void IsEven_ShouldReturnCorrectResult(int input, bool expected)
+    {
+        bool result = _evenNumber.FindEven(input);
+
+        Assert.AreEqual(expected, result);
+    }
+}
