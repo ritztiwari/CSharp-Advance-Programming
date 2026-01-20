314ffc52fc08d958457ed34d302b4b7a3b1f8a07
Rishabh Tiwari <mrishabh532@gmail.com>
2026-01-20 16:49:51 +0530

    [Rishabh]. Add database connections that checks & test connections and disconnection of database using NUnit.

---
+++ NUnit/DatabaseConnection/DatabaseConnectionTest.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using CSharp_Advance_Programming.NUnit.DatabaseConnection;
+
+[TestFixture]
+public class DatabaseConnectionTests
+{
+    private DatabaseConnection _db;
+
+    [SetUp]
+    public void Setup()
+    {
+        _db = new DatabaseConnection();
+        _db.Connect();               // Runs before EVERY test
+    }
+
+    [TearDown]
+    public void Cleanup()
+    {
+        _db.Disconnect();            // Runs after EVERY test
+    }
+
+    [Test]
+    public void Connect_ShouldEstablishConnection()
+    {
+        Assert.IsTrue(_db.IsConnected);
+    }
+
+    [Test]
+    public void Disconnect_ShouldCloseConnection()
+    {
+        _db.Disconnect();
+        Assert.IsFalse(_db.IsConnected);
+    }
+}
